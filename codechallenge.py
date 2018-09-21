f836b1359244953a077b922fbab96ccffd721124
Jason Zaitz <jasonzaitz1@gmail.com>
2018-09-21 16:16:50 -0400

    added other exercises

--- codechallenge.py
+++ codechallenge.py
@@ -6,3 +6,21 @@ def string_bits(str):
     if i % 2 == 0:
       newstr = newstr + str[i]
   return newstr
+
+#Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+def string_splosion(str):
+  newstr = ""
+  numtimes = len(str) + 1
+  for i in range (1, numtimes):
+    newstr = newstr + str[0:i]
+  return newstr
+
+#Given an array of ints, return the number of 9's in the array.
+
+def array_count9(nums):
+  counter = 0
+  for i in range(0,len(nums)):
+    if nums[i] == 9:
+      counter = counter + 1
+  return counter
