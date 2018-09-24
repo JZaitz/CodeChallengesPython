cf49d35678359c2afa75a9ad22e4db005eec441a
Jason Zaitz <jasonzaitz1@gmail.com>
2018-09-24 16:04:30 -0400

    added other exercises

--- codechallenge.py
+++ codechallenge.py
@@ -42,3 +42,16 @@ def array_count9(nums):
    if nums[i]==1 and nums[i+1]==2 and nums[i+2]==3:
      return True
   return False
+
+#Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+  def hello_name(name):
+  return "Hello %s!" % name
+
+#Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+def make_abba(a, b):
+  return a + b + b + a
+
+
+#The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+def make_tags(tag, word):
+  return "<" + tag + ">" + word + "</" + tag + ">"
