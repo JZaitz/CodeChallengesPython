7a53099fa4af8de9a3021149db13a91c071551af
Jason Zaitz <jasonzaitz1@gmail.com>
2018-09-25 13:51:02 -0400

    added other exercises

--- codechallenge.py
+++ codechallenge.py
@@ -65,4 +65,8 @@ def make_out_word(out, word):
   oute = out[2:]
   return outb + word + oute
 
-  
+
+#Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+def extra_end(str):
+  end = str[-2:]
+  return end * 3
