259292d1cfe60d089333c09710f418d6f570b127
Jason Zaitz <jasonzaitz1@gmail.com>
2018-09-25 13:54:01 -0400

    added other exercises

--- codechallenge.py
+++ codechallenge.py
@@ -70,3 +70,12 @@ def make_out_word(out, word):
 def extra_end(str):
   end = str[-2:]
   return end * 3
+
+
+#Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "".
+def first_two(str):
+  end = str[:2]
+  if len(str) >= 2:
+    return end
+  else:
+    return str[:1]
